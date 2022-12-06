58bc8cee11aa8dbad7bb16f5b7d80fcd8c706057
Giovani Moreira <giovani_moreira220@outlook.com>
2022-12-06 14:39:59 -0300

    Criando metodos http userAll e getById

--- src/main/java/com/cursospring/br/resourses/UserResource.java
+++ src/main/java/com/cursospring/br/resourses/UserResource.java
@@ -1,16 +1,34 @@
 package com.cursospring.br.resourses;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursospring.br.entities.UsuarioEntities;
+import com.cursospring.br.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class UserResource {
-
-	public ResponseEntity<UsuarioEntities> findAll(){
-		return null;
+	
+	@Autowired
+	private UsuarioService service;
+	
+	@GetMapping
+	public ResponseEntity<List<UsuarioEntities>> findAll(){
+		List<UsuarioEntities> listaUsuario = service.findAll();
+		return ResponseEntity.ok().body(listaUsuario);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UsuarioEntities> findById(@PathVariable Long id){
+		UsuarioEntities user = service.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
+	
 }
---
+++ src/main/java/com/cursospring/br/services/UsuarioService.java
@@ -0,0 +1,27 @@
+package com.cursospring.br.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursospring.br.entities.UsuarioEntities;
+import com.cursospring.br.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository repository;
+
+	public List<UsuarioEntities> findAll (){
+		return repository.findAll();
+	}
+	
+	public UsuarioEntities findById (Long id) {
+		Optional<UsuarioEntities> userObj = repository.findById(id);
+		return userObj.get();
+	}
+	
+}
