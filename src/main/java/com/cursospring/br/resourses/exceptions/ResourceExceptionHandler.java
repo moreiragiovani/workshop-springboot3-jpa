46426ff788f63a02e63a02ad82af36d616c55988
Giovani Moreira <giovani_moreira220@outlook.com>
2022-12-16 14:30:46 -0300

    Tratando exception delete

--- src/main/java/com/cursospring/br/resourses/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/cursospring/br/resourses/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.cursospring.br.services.exceptions.DatabaseException;
 import com.cursospring.br.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandartError> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found.";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);		
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandartError> dataBaseException(DatabaseException e, HttpServletRequest request){
+		String error = "Data base error.";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandartError err = new StandartError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);		
 	}
